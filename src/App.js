430fc8ca0eee8dfe364d6dd37e9d824c3317e6c4
rpsingh6634 <rpsingh6634@gmail.com>
2022-11-23 14:42:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Color from "./Components/Color";
 
 function App() {
+  const styling = {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={styling}>
+        <Color title="#FF6663" color="PINK" code="#FF6663" />
+        <Color title="#333333" color="GRAY" code="#333333" />
+        <Color title="#000000" color="BLACK" code="#000000" />
+        <Color title="#38BB14" color="GREEN" code="#38BB14" />
+        <Color title="#C90B0B" color="RED" code="#C90B0B" />
+      </div>
+
+      <div style={styling}>
+        <Color title="#FF8000" color="ORANGE" code="#FF8000" />
+        <Color title="#FFF500" color="YELLOW" code="#FFf500" />
+        <Color title="#CCCCCC" color="GRAY" code="#CCCCCC" />
+        <Color title="#7E41A2" color="PURPLE" code="#7E41A2" />
+        <Color title="#C14911" color="BROWN" code="#C14911" />
+      </div>
+    </>
   );
 }
 
